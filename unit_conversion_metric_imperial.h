d8ef1f5ce6213022d7442b5a65e8e3011113088f
etiennefournier <fournier_etienne@hotmail.com>
2020-01-03 21:05:02 -0500

    Removed usage of internal typedefs in metric/imperial conversion engine to ease future reuse.

--- unit_conversion_metric_imperial.h
+++ unit_conversion_metric_imperial.h
@@ -25,28 +25,28 @@ using thous_in_micrometers = ratio<5, 127>;
 // TODO see what needs to be done to support other flavors of num and den equality (last 2 templated arguments).
 
 template <class _RepFrom, class _DimensionFrom, class _RepTo, class _DimensionTo, class _Dimension>
-struct __unit_cast<metric_unit<_RepFrom, _DimensionFrom>, imperial_unit<_RepTo, _DimensionTo>, _Dimension, false, false>
+struct __unit_cast<__unit<metric_trait, _RepFrom, _DimensionFrom>, __unit<imperial_trait, _RepTo, _DimensionTo>, _Dimension, false, false>
 {
     _LIBCPP_INLINE_VISIBILITY _LIBCPP_CONSTEXPR
-    imperial_unit<_RepTo, _DimensionTo> operator()(const metric_unit<_RepFrom, _DimensionFrom>& __fu) const
+    __unit<imperial_trait, _RepTo, _DimensionTo> operator()(const __unit<metric_trait, _RepFrom, _DimensionFrom>& __fu) const
     {
         const auto refUnitFrom = unit_cast<micrometers>(__fu);
         const auto refUnitTo   = thous{refUnitFrom.count() * thous_in_micrometers::num / thous_in_micrometers::den};
 
-        return unit_cast<imperial_unit<_RepTo, _DimensionTo> >(refUnitTo);
+        return unit_cast<__unit<imperial_trait, _RepTo, _DimensionTo> >(refUnitTo);
     }
 };
 
 template <class _RepFrom, class _DimensionFrom, class _RepTo, class _DimensionTo, class _Dimension>
-struct __unit_cast<imperial_unit<_RepFrom, _DimensionFrom>, metric_unit<_RepTo, _DimensionTo>, _Dimension, false, false>
+struct __unit_cast<__unit<imperial_trait, _RepFrom, _DimensionFrom>, __unit<metric_trait, _RepTo, _DimensionTo>, _Dimension, false, false>
 {
     _LIBCPP_INLINE_VISIBILITY _LIBCPP_CONSTEXPR
-    metric_unit<_RepTo, _DimensionTo> operator()(const imperial_unit<_RepFrom, _DimensionFrom>& __fu) const
+    __unit<metric_trait, _RepTo, _DimensionTo> operator()(const __unit<imperial_trait, _RepFrom, _DimensionFrom>& __fu) const
     {
         const auto refUnitFrom = unit_cast<thous>(__fu);
         const auto refUnitTo   = micrometers{refUnitFrom.count() * thous_in_micrometers::den / thous_in_micrometers::num};
 
-        return unit_cast<metric_unit<_RepTo, _DimensionTo> >(refUnitTo);
+        return unit_cast<__unit<metric_trait, _RepTo, _DimensionTo> >(refUnitTo);
     }
 };
 
